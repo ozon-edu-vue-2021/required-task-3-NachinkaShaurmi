7d998a0afa4f07edad5982fa41d0b55d876613eb
Iurii Oleinik <ioleinik@emphasoft.com>
2021-10-31 17:01:34 +0300

    feat: install d3 and add tables to map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,23 +3,81 @@
     <h3>Карта офиса</h3>
 
     <div v-if="!isLoading" class="map-root">
-      <MapSVG />
+      <MapSVG ref="svg" />
+      <Table v-show="false" ref="table" />
     </div>
     <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
 import MapSVG from "@/assets/images/map.svg";
+import tables from "@/assets/data/tables.json";
+import Table from "@/assets/images/workPlace.svg";
+import legend from "@/assets/data/legend.json";
 
 export default {
   components: {
     MapSVG,
+    Table,
   },
   data() {
     return {
       isLoading: false,
+      svg: null,
+      g: null,
+      tableSVG: null,
+      tables: [],
     };
+  },
+  created() {
+    this.tables = tables;
+  },
+  mounted() {
+    this.isLoading = true;
+
+    this.svg = d3.select(this.$refs.svg);
+    this.g = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table);
+
+    if (this.g) {
+      this.drawTables();
+    } else {
+      console.error("SVG is incorrect");
+    }
+
+    this.isLoading = false;
+  },
+  methods: {
+    drawTables() {
+      // создаем группу для рабочих мест
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+
+      this.tables.map((table) => {
+        // создает группу для рабочего стола
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html())
+          .attr(
+            // устанавливаем цвет подразделения
+            "fill",
+            legend.find((it) => it.group_id === table.group_id)?.color ??
+              "transparent"
+          );
+      });
+    },
   },
 };
 </script>
